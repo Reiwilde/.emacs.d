6c7c391a1f7c2cf011ae9735f52965045b7e19d4
Alexis Morel <alexis.morel@adyoulike.com>
2019-02-03 00:57:41 +0100

    experimental

--- custom.el
+++ custom.el
@@ -3,10 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (spacemacs-theme))))
+ '(package-selected-packages (quote (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#292b2e" :foreground "#b2b2b2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "outline" :family "Fira Code")))))
+ '(default ((t (:inherit nil :stipple nil :background "#292b2e" :foreground "#b2b2b2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "DAMA" :family "Fira Code")))))
--- init.el
+++ init.el
@@ -1,34 +1,83 @@
-(set-language-environment "UTF-8")
+;;; package --- summary
+;;; Commentary:
+;;; Code:
 
-;; custom
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-
-;; melpa
-(require 'package)
-
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
-
-(package-initialize)
-
-;; display
-(load-theme 'spacemacs-dark t)
-
+;; Turn off mouse interface early in startup to avoid momentary display
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 
-;; typescript
-;;(add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
+(setq inhibit-startup-screen t)
 
-;;(defun setup-tide-mode ()
-;;  (interactive)
-;;  (tide-setup)
-;;  (flycheck-mode +1)
-;;  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-;;  (eldoc-mode +1)
-;;  (tide-hl-identifier-mode +1)
-;;  (company-mode +1))
+(set-language-environment "UTF-8")
 
-;;(add-hook 'typescript-mode-hook #' setup-tide-mode)
+;;; require
+(require 'package)
+
+;;; custom
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;;; melpa
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")
+			 ("melpa-stable" . "https://stable.melpa.org/packages/"))
+      package-archive-priorities '(("gnu" . 100)
+				   ("melpa" . 10000)
+				   ("melpa-stable" . 1000)))
+
+(package-initialize)
+
+(use-package spacemacs-theme
+  :ensure t
+  :defer t
+  :init
+  (load-theme 'spacemacs-dark t))
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode))
+
+(use-package projectile
+  :ensure t
+  :after (ivy)
+  :config
+  (setq projectile-completion-system 'ivy)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode))
+
+(use-package counsel
+  :ensure t
+  :after (ivy))
+
+(use-package counsel-projectile
+  :ensure t
+  :after (counsel projectile)
+  :config
+  (counsel-projectile-mode))
+
+(use-package company :ensure t)
+(use-package rainbow-delimiters :ensure t)
+(use-package rainbow-identifiers :ensure t)
+(use-package magit :ensure t)
+(use-package ensime :ensure t)
+(use-package tide :ensure t)
+
+(use-package elisp-mode
+  :after (company rainbow-delimiters rainbow-identifiers)
+  :hook (emacs-lisp-mode lambda ()
+			 (show-paren-mode) ; highlight parent delimiter
+			 (rainbow-delimiters-mode)
+			 (rainbow-identifiers-mode)
+			 (company-mode)))
+
+(setq backup-directory-alist (list (cons "." "~/.emacs.d/backups/")))
+
+(global-linum-mode) ; enable line number
+
